0be884b293503699470e5566ebd98017e0991446
Wayne <redwayne1000@gmail.com>
2026-04-13 21:51:53 +0100

    modal initiation

---
+++ src/components/Modal.tsx
@@ -0,0 +1,102 @@
+import type { Analysis } from "./dashboard";
+import { useRef } from "react";
+import { toPng } from "html-to-image";
+import { FaTimes } from "react-icons/fa";   // Font Awesome
+import { FiDownload } from "react-icons/fi";
+export default function Modal({ setModal, analysis }: { setModal: (modal: Boolean) => void, analysis: Analysis }) {
+    const cardRef = useRef<HTMLDivElement>(null);
+
+    const handleDownload = async () => {
+        if (!cardRef.current) return;
+
+        try {
+            const dataUrl = await toPng(cardRef.current, {
+                cacheBust: true,
+                pixelRatio: 2, // higher quality
+            });
+
+            const link = document.createElement("a");
+            link.download = "card.png";
+            link.href = dataUrl;
+            link.click();
+        } catch (err) {
+            console.error("Download failed:", err);
+        }
+    }
+    return (
+        <section className="fixed inset-0 z-50">
+            <div
+                className="absolute inset-0 w-full h-full bg-[#301014]/80 backdrop-blur-sm transition-opacity duration-300"
+                onClick={() => setModal(false)}
+            ></div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="">
+                    <div id="summaryreport" ref={cardRef} className="bg-[#51291E] max-w-[410px] md:min-w-[480px] md:max-w-[100vw] py-8 px-6  md:px-12 md:py-12 rounded-3xl shadow-2xl shadow-[#000000]/40 border border-[#EDF4ED]/10 text-[#EDF4ED] animate-[modalIn_0.25s_ease-out]">
+
+                        {/* Top row */}
+                        <div className="flex justify-around gap-6 items-baseline pb-7 mb-7 border-b border-[#EDF4ED]/15">
+                            <div className="flex gap-2">
+                                <span className="text-xl md:text-3xl font-bold text-[#79B791] tracking-wide max-w-[150px] md:max-w-[250px] overflow-hidden text-ellipsis whitespace-nowrap inline-block">{analysis.users[0]}</span>
+                                <span className="text-[#ABD1B5]/60 mx-3 text-lg">×</span>
+                                <span className="text-xl md:text-3xl font-bold text-[#79B791] tracking-wide max-w-[150px] md:max-w-[250px] overflow-hidden text-ellipsis whitespace-nowrap inline-block">{analysis.users[1]}</span>
+                            </div>
+                            <div className="flex flex-col md:flex-row items-baseline gap-2">
+                                <span className="text-xl md:text-4xl font-extrabold text-[#EDF4ED]">{analysis.total_length}</span>
+                                <span className="text-sm font-medium text-[#ABD1B5]/70 uppercase tracking-widest">msgs</span>
+                            </div>
+                        </div>
+
+                        {/* bottom part with image */}
+                        <div className="flex flex-row items-center gap-10">
+                            <div className="flex flex-col gap-5">
+                                <div className="flex items-center gap-3">
+                                    <span className="text-sm md:text-lg font-medium text-[#ABD1B5]">Longest Streak</span>
+                                    <span className="text-[#ABD1B5]/30">·</span>
+                                    <span className="flex gap-1 text-sm md:text-xl font-bold text-[#79B791]"> <span className="text-[#ABD1B5] italic">{analysis.longest_streak}</span>  days</span>
+                                </div>
+                                <div className="flex items-center gap-3">
+                                    <span className="text-sm md:text-lg font-medium text-[#ABD1B5]">Average Messages</span>
+                                    <span className="text-[#ABD1B5]/30">·</span>
+                                    <span className="flex gap-1 text-sm md:text-xl font-bold text-[#79B791]"> <span className="text-[#ABD1B5] italic">{Math.round(analysis.average_messages_per_day)}</span> msgs</span>
+                                </div>
+                                <div className="flex items-center gap-3">
+                                    <span className="text-sm md:text-lg font-medium text-[#ABD1B5]">Most Active Day</span>
+                                    <span className="text-[#ABD1B5]/30">·</span>
+                                    <span className="flex gap-1 text-sm md:text-xl font-bold text-[#79B791]"> <span className="text-[#ABD1B5] italic">{analysis.message_stats.max_messages}</span> msgs</span>
+                                </div>
+                            </div>
+                            <div className="ml-auto shrink-0">
+                                <img
+                                    src="/favicon.png"
+                                    alt="img"
+                                    className="w-28 h-28 rounded-2xl object-cover shadow-lg"
+                                />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="flex gap-2 justify-around mt-18">
+                        <button
+                            onClick={handleDownload}
+                            className=" flex gap-1 items-center bg-[#51291E]  px-6 py-3  text-white rounded-xl font-semibold hover:bg-[#51291E]/90 transition-colors"
+                        >
+                            <FiDownload className="text-xl md:text-3xl text-[#79B791]" />
+                            <span className="text-sm md:text-2xl">Download</span>
+                        </button>
+                        <a href="https://x.com/waynethefuture"></a>
+                        <button
+                            onClick={() => setModal(false)}
+                            className=" flex gap-1 items-center bg-[#51291E]  px-6 py-3  text-white rounded-xl font-semibold hover:bg-[#51291E]/90 transition-colors"
+                        >
+                            <FaTimes className="text-xl md:text-3xl text-[#79B791]" />
+                            <span className="text-sm md:text-2xl">
+                                Quote
+                            </span>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Bar, Pie } from "react-chartjs-2";
+import { FaUpload } from "react-icons/fa";
+import Modal from "./Modal.tsx";
+
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -12,7 +15,7 @@ import {
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, ArcElement, Tooltip, Legend);
 
-type Analysis = {
+export type Analysis = {
     users: string[];
     message_stats: {
         longest_day: string | null;
@@ -43,6 +46,7 @@ type Props = {
 const Dashboard: React.FC<Props> = ({ analysis }) => {
     const mostCommon = analysis.word_stats?.most_common?.slice(0, 5) ?? [];
     const leastCommon = analysis.word_stats?.least_common?.slice(0, 5) ?? [];
+    const [modal, setModal] = useState<Boolean>(false)
 
     const mostData = {
         labels: mostCommon.map(w => w[0]),
@@ -81,120 +85,131 @@ const Dashboard: React.FC<Props> = ({ analysis }) => {
     const silence = analysis.longest_silence;
 
     return (
-        <div className="min-h-screen bg-[#301014] p-6 text-[#EDF4ED]">
-            <div className="max-w-6xl flex flex-col mx-auto space-y-6">
+        <>
+            <div className="min-h-screen bg-[#301014] p-6 text-[#EDF4ED]">
+                <div className="max-w-6xl flex flex-col mx-auto space-y-6">
 
-                {/* Header */}
-                <div className="bg-[#51291E] rounded-2xl shadow-lg p-6 text-center">
-                    <h1 className="text-2xl font-bold text-[#79B791]">
-                        {analysis.users?.[0] ?? "User 1"} × {analysis.users?.[1] ?? "User 2"}
-                    </h1>
-                    <p className="text-[#ABD1B5] text-sm mt-1">
-                        Whatsapp Chat Analytics Dashboard
-                    </p>
+                    {/* Header */}
+                    <div className="flex justify-around items-center bg-[#51291E] rounded-2xl shadow-lg">
+                        <div className=" p-6 text-center">
+                            <h1 className="text-2xl font-bold text-[#79B791]">
+                                {analysis.users?.[0] ?? "User 1"} ×  <span className="text-xl md:text-3xl font-bold text-heading tracking-wide max-w-[150px] md:max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap inline-block">{analysis.users?.[1] ?? "User 2"}</span>
+                            </h1>
+                            <p className="text-[#ABD1B5] text-sm mt-1">
+                                Whatsapp Chat Analytics Dashboard
+                            </p>
+                        </div>
+                        <div className="p-2">
+                            <button className=" flex items-center bg-[#79B791] text-[#301014] hover:bg-[#79B791]/80 px-4 py-2 rounded-lg font-medium transition"
+                                onClick={() => setModal(true)}>
+                                <FaUpload className="mr-2 text-[#301014]" /> <span className="mr-2">Export</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Top Stats */}
+                    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 text-sm">
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <h2 className="font-semibold mb-2 text-[#79B791]">First Message</h2>
+                            <p>
+                                {analysis.first_message
+                                    ? new Date(analysis.first_message).toLocaleString()
+                                    : "N/A"}
+                            </p>
+                        </div>
+
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <h2 className="font-semibold mb-2 text-[#79B791]">Total Messages</h2>
+                            <p>{analysis.total_length ?? 0}</p>
+                        </div>
+                    </div>
+
+                    {/* Stats */}
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <p className="text-[#ABD1B5] text-sm">Longest Streak</p>
+                            <h2 className="text-xl font-semibold">
+                                {analysis.longest_streak ?? 0} days
+                            </h2>
+                        </div>
+
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <p className="text-[#ABD1B5] text-sm">Average Messages</p>
+                            <h2 className="text-xl font-semibold">
+                                {(analysis.average_messages_per_day ?? 0).toFixed(1)}
+                            </h2>
+                        </div>
+
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <p className="text-[#ABD1B5] text-sm">Longest Silence</p>
+                            <h2 className="text-sm font-medium">
+                                {silence?.duration ?? "No data"}
+                            </h2>
+                        </div>
+                    </div>
+
+                    {/* Activity Days */}
+                    <div className="grid md:grid-cols-2 gap-4">
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <p className="text-[#ABD1B5] text-sm">Most Active Day</p>
+                            <h2 className="font-semibold">
+                                {analysis.message_stats?.longest_day ?? "N/A"}
+                            </h2>
+                            <p className="text-sm">
+                                {analysis.message_stats?.max_messages ?? 0} messages
+                            </p>
+                        </div>
+
+                        <div className="bg-[#51291E] p-4 rounded-xl shadow">
+                            <p className="text-[#ABD1B5] text-sm">Least Active Day</p>
+                            <h2 className="font-semibold">
+                                {analysis.message_stats?.shortest_day ?? "N/A"}
+                            </h2>
+                            <p className="text-sm">
+                                {analysis.message_stats?.min_messages ?? 0} messages
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* Charts */}
+                    <div className="grid md:grid-cols-2 gap-6">
+                        <div className="bg-[#51291E] p-5 rounded-2xl shadow">
+                            <h2 className="font-semibold mb-4 text-[#79B791]">Most Common Words</h2>
+                            <Bar data={mostData} />
+                        </div>
+
+                        <div className="bg-[#51291E] p-5 rounded-2xl shadow">
+                            <h2 className="font-semibold mb-4 text-[#79B791]">Least Common Words</h2>
+                            <Bar data={leastData} />
+                        </div>
+                    </div>
+
+                    {/* Bottom */}
+                    <div className="grid md:grid-cols-2 gap-6">
+                        <div className="bg-[#51291E] p-5 rounded-2xl shadow">
+                            <h2 className="font-semibold mb-4 text-[#79B791]">
+                                Conversation Starters
+                            </h2>
+
+                            {conversationData ? (
+                                <Pie data={conversationData} />
+                            ) : (
+                                <p>No data</p>
+                            )}
+                        </div>
+
+                        <div className="bg-[#51291E] p-5 rounded-2xl shadow text-sm">
+                            <h2 className="font-semibold mb-2 text-[#79B791]">Silence Period</h2>
+                            <p>Start: {silence?.start ?? "N/A"}</p>
+                            <p>End: {silence?.end ?? "N/A"}</p>
+                        </div>
+                    </div>
+
                 </div>
-
-                {/* Top Stats */}
-                <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 text-sm">
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <h2 className="font-semibold mb-2 text-[#79B791]">First Message</h2>
-                        <p>
-                            {analysis.first_message
-                                ? new Date(analysis.first_message).toLocaleString()
-                                : "N/A"}
-                        </p>
-                    </div>
-
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <h2 className="font-semibold mb-2 text-[#79B791]">Total Messages</h2>
-                        <p>{analysis.total_length ?? 0}</p>
-                    </div>
-                </div>
-
-                {/* Stats */}
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <p className="text-[#ABD1B5] text-sm">Longest Streak</p>
-                        <h2 className="text-xl font-semibold">
-                            {analysis.longest_streak ?? 0} days
-                        </h2>
-                    </div>
-
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <p className="text-[#ABD1B5] text-sm">Average Messages</p>
-                        <h2 className="text-xl font-semibold">
-                            {(analysis.average_messages_per_day ?? 0).toFixed(1)}
-                        </h2>
-                    </div>
-
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <p className="text-[#ABD1B5] text-sm">Longest Silence</p>
-                        <h2 className="text-sm font-medium">
-                            {silence?.duration ?? "No data"}
-                        </h2>
-                    </div>
-                </div>
-
-                {/* Activity Days */}
-                <div className="grid md:grid-cols-2 gap-4">
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <p className="text-[#ABD1B5] text-sm">Most Active Day</p>
-                        <h2 className="font-semibold">
-                            {analysis.message_stats?.longest_day ?? "N/A"}
-                        </h2>
-                        <p className="text-sm">
-                            {analysis.message_stats?.max_messages ?? 0} messages
-                        </p>
-                    </div>
-
-                    <div className="bg-[#51291E] p-4 rounded-xl shadow">
-                        <p className="text-[#ABD1B5] text-sm">Least Active Day</p>
-                        <h2 className="font-semibold">
-                            {analysis.message_stats?.shortest_day ?? "N/A"}
-                        </h2>
-                        <p className="text-sm">
-                            {analysis.message_stats?.min_messages ?? 0} messages
-                        </p>
-                    </div>
-                </div>
-
-                {/* Charts */}
-                <div className="grid md:grid-cols-2 gap-6">
-                    <div className="bg-[#51291E] p-5 rounded-2xl shadow">
-                        <h2 className="font-semibold mb-4 text-[#79B791]">Most Common Words</h2>
-                        <Bar data={mostData} />
-                    </div>
-
-                    <div className="bg-[#51291E] p-5 rounded-2xl shadow">
-                        <h2 className="font-semibold mb-4 text-[#79B791]">Least Common Words</h2>
-                        <Bar data={leastData} />
-                    </div>
-                </div>
-
-                {/* Bottom */}
-                <div className="grid md:grid-cols-2 gap-6">
-                    <div className="bg-[#51291E] p-5 rounded-2xl shadow">
-                        <h2 className="font-semibold mb-4 text-[#79B791]">
-                            Conversation Starters
-                        </h2>
-
-                        {conversationData ? (
-                            <Pie data={conversationData} />
-                        ) : (
-                            <p>No data</p>
-                        )}
-                    </div>
-
-                    <div className="bg-[#51291E] p-5 rounded-2xl shadow text-sm">
-                        <h2 className="font-semibold mb-2 text-[#79B791]">Silence Period</h2>
-                        <p>Start: {silence?.start ?? "N/A"}</p>
-                        <p>End: {silence?.end ?? "N/A"}</p>
-                    </div>
-                </div>
-
             </div>
-        </div>
+            {modal && <Modal setModal={setModal} analysis={analysis} />}
+        </>
     );
 };
 
-export default Dashboard;
+export default Dashboard
